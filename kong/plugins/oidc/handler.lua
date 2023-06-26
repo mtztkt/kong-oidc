4d855e799f0e354989a811bf14cae3d8d18c0a03
mustafaaozcan <mustafaaozcan@gmail.com>
2023-06-26 09:48:48 +0300

    Update handler.lua

--- kong/plugins/oidc/handler.lua
+++ kong/plugins/oidc/handler.lua
@@ -5,6 +5,7 @@ local OidcHandler = {
 local utils = require("kong.plugins.oidc.utils")
 local filter = require("kong.plugins.oidc.filter")
 local session = require("kong.plugins.oidc.session")
+local openidc = require("kong.plugins.oidc.openidc")
 
 
 function OidcHandler:access(config)
@@ -93,7 +94,7 @@ function make_oidc(oidcConfig)
     -- constant for resty.oidc library
     unauth_action = "deny"
   end
-  local res, err = require("resty.openidc").authenticate(oidcConfig, ngx.var.request_uri, unauth_action,session_opts)
+  local res, err = openidc.authenticate(oidcConfig, ngx.var.request_uri, unauth_action,session_opts)
 
   if err then
     if err == 'unauthorized request' then
@@ -113,9 +114,9 @@ function introspect(oidcConfig)
   if utils.has_bearer_access_token() or oidcConfig.bearer_only == "yes" then
     local res, err
     if oidcConfig.use_jwks == "yes" then
-      res, err = require("resty.openidc").bearer_jwt_verify(oidcConfig)
+      res, err = openidc.bearer_jwt_verify(oidcConfig)
     else
-      res, err = require("resty.openidc").introspect(oidcConfig)
+      res, err = openidc.introspect(oidcConfig)
     end
     if err then
       if oidcConfig.bearer_only == "yes" then
@@ -159,7 +160,7 @@ function verify_bearer_jwt(oidcConfig)
     ssl_verify = oidcConfig.ssl_verify
   }
 
-  local discovery_doc, err = require("resty.openidc").get_discovery_doc(opts)
+  local discovery_doc, err = openidc.get_discovery_doc(opts)
   if err then
     kong.log.err('Discovery document retrieval for Bearer JWT verify failed')
     return nil
@@ -180,7 +181,7 @@ function verify_bearer_jwt(oidcConfig)
     nbf = jwt_validators.opt_is_not_before(),
   }
 
-  local json, err, token = require("resty.openidc").bearer_jwt_verify(opts, claim_spec)
+  local json, err, token = openidc.bearer_jwt_verify(opts, claim_spec)
   if err then
     kong.log.err('Bearer JWT verify failed: ' .. err)
     return nil
