1e61ba68372c493920dd5638353129885d252571
mustafa.ozcan <mustafa.ozcan@dgpays.com>
2023-07-12 14:25:14 +0300

    .

--- kong/plugins/oidc/openidc.lua
+++ kong/plugins/oidc/openidc.lua
@@ -386,9 +386,7 @@ local function openidc_authorize(opts, session, target_url, prompt)
 
   -- redirect to the /authorization endpoint
   ngx.header["Cache-Control"] = "no-cache, no-store, max-age=0"
-  --opts.discovery.authorization_endpoint
-  local authorization_endpoint = "http://192.168.1.46:8080/realms/dgpaysit/protocol/openid-connect/auth"
-  return ngx.redirect(openidc_combine_uri(authorization_endpoint, params))
+  return ngx.redirect(openidc_combine_uri(opts.discovery.authorization_endpoint, params))
 end
 
 -- parse the JSON result from a call to the OP
