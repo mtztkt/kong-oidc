340217a5bc8d0b3e931c70870733ee9d1571510d
mustafa.ozcan <mustafa.ozcan@dgpays.com>
2023-07-25 02:55:16 +0300

    .

--- kong/plugins/oidc/filter.lua
+++ kong/plugins/oidc/filter.lua
@@ -18,7 +18,10 @@ local function shouldIgnoreRequestMethod(patterns)
   if (patterns) then
     for _, pattern in ipairs(patterns) do
       local isMatching = not (string.find(ngx.var.request_method, pattern) == nil)
-      if (isMatching) then return true end
+      if (isMatching) then 
+        ngx.log(ngx.DEBUG, "121212OidcHandler ignoring request method: ".. ngx.var.request_method)
+        return true 
+      end
     end
   end
   return false
