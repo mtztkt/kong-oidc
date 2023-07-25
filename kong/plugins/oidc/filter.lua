105a12f5e146d82789749f792fa141c8bb933fac
mustafa.ozcan <mustafa.ozcan@dgpays.com>
2023-07-25 03:24:47 +0300

    .

--- kong/plugins/oidc/filter.lua
+++ kong/plugins/oidc/filter.lua
@@ -15,16 +15,10 @@ function M.shouldProcessRequest(config)
 end
 
 local function shouldIgnoreRequestMethod(patterns)
-  ngx.log(ngx.DEBUG, "5121212OidcHandler ignoring request method: ".. ngx.var.request_method)
   if (patterns) then
-    ngx.log(ngx.DEBUG, "3121212OidcHandler ignoring request method: ".. ngx.var.request_method)
     for _, pattern in ipairs(patterns) do
-      ngx.log(ngx.DEBUG, "421212OidcHandler ignoring request method: ".. ngx.var.request_method)
       local isMatching = not (string.find(ngx.var.request_method, pattern) == nil)
-      if (isMatching) then 
-        ngx.log(ngx.DEBUG, "121212OidcHandler ignoring request method: ".. ngx.var.request_method)
-        return true 
-      end
+      if (isMatching) then return true end
     end
   end
   return false
