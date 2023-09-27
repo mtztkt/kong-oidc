5b48ca8ee76a734a27147be9c722d8fee20c6248
mustafaaozcan <mustafaaozcan@gmail.com>
2023-09-27 15:02:39 +0300

    Update api.lua

--- kong/plugins/oidc/api.lua
+++ kong/plugins/oidc/api.lua
@@ -9,7 +9,7 @@ local function find_plugin()
         return nil, err
       end
   
-      if plugin.name == "oidc" and plugin.service_id == nil then
+      if plugin.name == "oidc" then
         return plugin
       end
     end
