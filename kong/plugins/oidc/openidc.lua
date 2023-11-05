b8f19ce041fe1d10079c721477c7290f19dd04cb
mustafa.ozcan <mustafa.ozcan@dgpays.com>
2023-11-06 01:03:32 +0300

    .

--- kong/plugins/oidc/openidc.lua
+++ kong/plugins/oidc/openidc.lua
@@ -1375,7 +1375,7 @@ local function openidc_logout(opts, session)
 
       if not session_token then
         log(ERROR, "session_token not found logout redirect :", opts.post_logout_redirect_uri)
-        return ngx.redirect(opts.post_logout_redirect_uri)
+        return openidc_authorize(opts, session, opts.logout_path, "none")
       end
 
       params["id_token_hint"] = session_token
